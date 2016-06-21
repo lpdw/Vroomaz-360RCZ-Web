7cc7deac8f058e227bc4955ed3d9020bb5af637c
Maxime Florile <florile.maxime@gmail.com>
2016-06-21 16:18:27 +0200

    Init /SITE (npm, gulp), + gitignore

---
+++ SITE/gulpfile.js
@@ -0,0 +1,60 @@
+// Dependencies
+var gulp = require('gulp'),
+livereload = require('gulp-livereload'),
+plumber = require('gulp-plumber'),
+sassGlob = require('gulp-sass-glob'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+sass = require('gulp-sass'),
+gcmq = require('gulp-group-css-media-queries');
+
+//Paths
+var paths = {
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dest: 'dist/css'
+  },
+  scripts: {
+    src: [],
+    dest: 'dist/js'
+  },
+};
+
+// Notify
+var notifyInfo = {
+  title: 'Gulp'
+};
+
+//Plumber
+var plumberErrorHandler = { errorHandler: notify.onError({
+  title: notifyInfo.title,
+  icon: notifyInfo.icon,
+  message: "Error: <%= error.message %>"
+})
+};
+
+// Styles
+gulp.task('styles', function () {
+
+  return gulp.src(paths.styles.src)
+  .pipe(plumber(plumberErrorHandler))
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle : 'expanded' }))
+  .pipe(concat( 'main.css' ))
+  .pipe(gcmq())
+  .pipe(gulp.dest(paths.styles.dest));
+
+});
+
+// Watch
+gulp.task('watch', function () {
+
+  livereload.listen();
+
+  // Watch .scss files
+  gulp.watch(paths.styles.src, ['styles']);
+
+  // Watch .js files
+  gulp.watch(paths.scripts.src, ['scripts']);
+
+});
