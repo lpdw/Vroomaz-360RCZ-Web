d1a5b6436b8c142eccffb3699c2301f2a326a27c
Maxime Florile <florile.maxime@gmail.com>
2016-06-24 16:51:30 +0200

    /API start routing

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* index */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* FORWARD */
+router.post('/forward', function(req, res) {
+
+});
+
+/* BACKWARD */
+router.post('/backward', function(req, res) {
+
+});
+
+/* LEFT */
+router.post('/left', function(req, res) {
+
+});
+
+/* RIGHT */
+router.post('/right', function(req, res) {
+
+});
+
+/* STOP */
+router.post('/stop', function(req, res) {
+
+});
+
+/* POST CONFIG */
+router.post('/config', function(req, res) {
+  if (req.accepts('application/json')) {
+    return res.status(200).send(req.body);
+  }
+});
+
+/* GET CONFIG */
+router.get('/config', function(req, res) {
+
+});
+
+/* POST MODE */
+router.post('/mode', function(req, res) {
+  if (req.accepts('application/json')) {
+    return res.status(200).send(req.body);
+  }
+});
+
+/* GET MODE */
+router.get('/mode', function(req, res) {
+
 });
 
 module.exports = router;
