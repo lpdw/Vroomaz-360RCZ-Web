2287cc1355246da94c269a2ebf002765efa47225
Enguerran P <contact@epoulain.fr>
2016-06-26 23:24:20 +0200

    /SITE Add basic controller support (use directionnal pad)

--- SITE/src/javascripts/manualCommand.js
+++ SITE/src/javascripts/manualCommand.js
@@ -1,7 +1,7 @@
 var lastEvent;
 var controlServer = "http://192.168.1.240";
 
-function go() {
+function start() {
   $.ajax({
     url: controlServer + "/start"
   })
@@ -39,7 +39,7 @@ function left() {
 
 function right() {
   $.ajax({
-    url: controlServer + "/left"
+    url: controlServer + "/right"
   })
   .done(function (msg) {
     console.log(msg);
@@ -61,7 +61,7 @@ $(document).keydown(function(e) {
 
       case 38: // forward
         console.log('forward');
-        back();
+        start();
         lastEvent = e;
         break;
 
@@ -73,7 +73,7 @@ $(document).keydown(function(e) {
 
       case 40: // backward
         console.log('backward');
-        go();
+        back();
         lastEvent = e;
         break;
 
@@ -118,4 +118,165 @@ $(document).keyup(function(e) {
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
   }
+});
+
+// GAMEPAD SUPPORT
+
+var hasGP = false;
+var repGP;
+var lastEventGP;
+
+function canGame() {
+  return "getGamepads" in navigator;
+}
+
+function reportOnGamepad() {
+  var gp = navigator.getGamepads()[0];
+  var html = "";
+  var xinput = {
+    0: "A",
+    1: "B",
+    2: "X",
+    3: "Y",
+    4: "LB",
+    5: "RB",
+    6: "LT",
+    7: "RT",
+    8: "BACK",
+    9: "START",
+    10: "LSTICK",
+    11: "RSTICK",
+    12: "UP",
+    13: "DOWN",
+    14: "LEFT",
+    15: "RIGHT"
+  };
+
+  for(var i=0;i<gp.buttons.length;i++) {
+
+    html+= "Button "+ xinput[i] +" (" + i +") :";
+
+    if(gp.buttons[i].pressed) {
+
+      html+= " pressed";
+
+      if (lastEventGP) {
+        html+= "<br/>";
+        continue;
+      }
+
+      switch(xinput[i]) {
+        case "LEFT": // left
+          console.log('left');
+          left();
+          lastEventGP = i;
+          break;
+
+        case "UP": // forward
+          console.log('forward');
+          start();
+          lastEventGP = i;
+          break;
+
+        case "RIGHT": // right
+          console.log('right');
+          right();
+          lastEventGP = i;
+          break;
+
+        case "DOWN": // backward
+          console.log('backward');
+          back();
+          lastEventGP = i;
+          break;
+
+        default:
+          break;
+      }
+
+    } else { //if button
+
+      switch(xinput[i]) {
+        case "LEFT": // left
+          if (lastEventGP == i) {
+            console.log('stop left');
+            stop();
+            lastEventGP = null;
+          }
+          break;
+
+        case "UP": // up
+          if (lastEventGP == i) {
+            console.log('stop up');
+            stop();
+            lastEventGP = null;
+          }
+          break;
+
+        case "RIGHT": // right
+          if (lastEventGP == i) {
+            console.log('stop right');
+            stop();
+            lastEventGP = null;
+          }
+          break;
+
+        case "DOWN": // down
+          if (lastEventGP == i) {
+            console.log('stop down');
+            stop();
+            lastEventGP = null;
+          }
+          break;
+
+        default:
+          break;
+      }
+
+    }
+
+    html+= "<br/>";
+  }
+
+  // Pour les axes analogiques
+  for(var i=0;i<gp.axes.length; i+=2) {
+    html+= "Stick "+(Math.ceil(i/2)+1)+": "+gp.axes[i]+","+gp.axes[i+1]+"<br/>";
+  }
+
+  $("#gamepadDisplay").html(html);
+  //console.log(html);
+}
+
+$(document).ready(function() {
+
+  if(canGame()) {
+
+    var gamepad_state = "Not connected";
+
+   $("#state").text(gamepad_state);
+    console.log(gamepad_state);
+
+    $(window).on("gamepadconnected", function() {
+      hasGP = true;
+      $("#state").html("Gamepad connected!");
+      console.log("connection event");
+      repGP = window.setInterval(reportOnGamepad,100);
+    });
+
+    $(window).on("gamepaddisconnected", function() {
+      console.log("disconnection event");
+      $("#state").text(gamepad_state);
+      window.clearInterval(repGP);
+    });
+
+    //setup an interval for Chrome
+    // var checkGP = window.setInterval(function() {
+    //     console.log('checkGP');
+    //     if(navigator.getGamepads()[0]) {
+    //         if(!hasGP) $(window).trigger("gamepadconnected");
+    //         window.clearInterval(checkGP);
+    //     }
+    // }, 500);
+  }
+
 });
